e75bcf73aae63e0989977b189502937ea9071237
Pedro Paulo Ventura Tecchio <pedrotecchio@gmail.com>
2020-09-20 16:26:13 -0300

    added code, data and some figures

---
+++ code/helicoptero_cria_experimento_modelagem.R
@@ -0,0 +1,9 @@
+#cria um experimento randomico para modelar o helicoptero
+helicopterodesign <- expand.grid(Altura = gl(2,1, labels = c("1.30","2.10")),
+                                 Clipe = gl(2,1, labels = c("-","+")),
+                                 AdesivoTopo = gl(2,1, labels = c("-","+")),
+                                 AdesivoLateral = gl(2,1, labels = c("-","+")))
+helicopterodesign
+helicopterodesign$ordem <- sample(1:16,16)
+helicopterodesign <- helicopterodesign[order(helicopterodesign$ordem),]
+helicopterodesign
